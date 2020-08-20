075de284ce6a447063ab0c56e581f4cc1d5c27cf
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-08-20 01:04:07 -0300

    Creates gulp task to copy `fontawesome` from `node_modules` to `source` folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-function defaultTask(cb) {
-  cb();
-}
+const { dest, src, series, task } = require('gulp');
 
-exports.default = defaultTask
+
+task('source:fonts:fontAwesome', function() {
+  return src(
+    [
+      'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
+      'node_modules/@fortawesome/fontawesome-free/webfonts/*',
+    ],
+    {base: 'node_modules/@fortawesome/fontawesome-free'},
+  )
+  .pipe(dest('./source/lib/font-awesome'));
+});
+
+
+task('source', series('source:fonts:fontAwesome'));
